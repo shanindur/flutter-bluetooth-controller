8d6a43e6910b4b7bf8d83d83238780f04d70067a
marpakprivatelimited <shanindu.rajapaksha91@gmail.com>
2020-09-18 00:04:29 +0530

    Started vocal commands

--- application/lib/screens/vocal_command_config.dart
+++ application/lib/screens/vocal_command_config.dart
@@ -1,4 +1,8 @@
+import 'package:application/model/app_state.dart';
+import 'package:application/redux/actions.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 class VocalCommandConfig extends StatefulWidget {
   @override
@@ -10,24 +14,245 @@ class _VocalCommandConfigState extends State<VocalCommandConfig> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Vocal Commands",
+        title: Text(
+          "Vocal Commands",
           style: TextStyle(
             fontFamily: 'Lato',
-          ),),
+          ),
+        ),
         centerTitle: true,
         backgroundColor: Color(0xFF00979d),
       ),
-      body: Container(
-        child: Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: Container(
-            alignment: Alignment.bottomRight,
-            child: FloatingActionButton(
-              onPressed: (){},
-              child: Icon(Icons.add),
+      body: StoreConnector<AppState, AppState>(
+        converter: (store) => store.state,
+        builder: (context, state) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Alert(
+                      context: context,
+                      type: AlertType.info,
+                      title: "Update  ",
+                      desc: "A new update is available.",
+                      buttons: [],
+                    ).show();
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Align(
+                          alignment: Alignment.topLeft,
+                          child: Text(
+                            'Command 01',
+                            textAlign: TextAlign.left,
+                            style:
+                                TextStyle(fontFamily: 'Lato', fontSize: 20.0),
+                          ),
+                        ),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.grey,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Divider(
+                  color: Colors.black,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          'Command 02',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(fontFamily: 'Lato', fontSize: 20.0),
+                        ),
+                      ),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.grey,
+                      ),
+                    ],
+                  ),
+                ),
+                Divider(
+                  color: Colors.black,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          'Command 03',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(fontFamily: 'Lato', fontSize: 20.0),
+                        ),
+                      ),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.grey,
+                      ),
+                    ],
+                  ),
+                ),
+                Divider(
+                  color: Colors.black,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          'Command 05',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(fontFamily: 'Lato', fontSize: 20.0),
+                        ),
+                      ),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.grey,
+                      ),
+                    ],
+                  ),
+                ),
+                Divider(
+                  color: Colors.black,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          'Command 06',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(fontFamily: 'Lato', fontSize: 20.0),
+                        ),
+                      ),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.grey,
+                      ),
+                    ],
+                  ),
+                ),
+                Divider(
+                  color: Colors.black,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          'Command 07',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(fontFamily: 'Lato', fontSize: 20.0),
+                        ),
+                      ),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.grey,
+                      ),
+                    ],
+                  ),
+                ),
+                Divider(
+                  color: Colors.black,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          'Command 08',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(fontFamily: 'Lato', fontSize: 20.0),
+                        ),
+                      ),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.grey,
+                      ),
+                    ],
+                  ),
+                ),
+                Divider(
+                  color: Colors.black,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          'Command 09',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(fontFamily: 'Lato', fontSize: 20.0),
+                        ),
+                      ),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.grey,
+                      ),
+                    ],
+                  ),
+                ),
+                Divider(
+                  color: Colors.black,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          'Command 10',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(fontFamily: 'Lato', fontSize: 20.0),
+                        ),
+                      ),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.grey,
+                      ),
+                    ],
+                  ),
+                ),
+                Divider(
+                  color: Colors.black,
+                ),
+              ],
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
